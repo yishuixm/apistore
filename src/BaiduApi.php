c2abaae243953b8a3120fe04aa061a86721ae1ba
yishuixm <1830802211@qq.com>
2016-09-27 15:57:57 +0800

    更新手机号归属地查询

--- src/BaiduApi.php
+++ src/BaiduApi.php
@@ -45,11 +45,26 @@ class BaiduApi
      * URL: http://apistore.baidu.com/apiworks/servicedetail/735.html
      * Date: 2016/9/27
      * Author: 1830802211@qq.com
-     * @param $apikey
+     * @param $apikey 百度APIKEY
      * @param $cardnum 身份证号
      */
     public function apistore_idservice_id($apikey, $id){
         $url = self::API_URL.implode("/",explode('_', __FUNCTION__));
         return $this->remoteApi($url,["apikey:{$apikey}"],http_build_query(["id"=>$id]));
     }
+
+    /**
+     * 身份证查询
+     * URL: http://apistore.baidu.com/apiworks/servicedetail/735.html
+     * Date: 2016/9/27
+     * Author: 1830802211@qq.com
+     * @param $apikey 百度APIKEY
+     * @param $phone 手机号码
+     */
+    public function apistore_mobilenumber_mobilenumber($apikey, $phone){
+        $url = self::API_URL.implode("/",explode('_', __FUNCTION__));
+        return $this->remoteApi($url,["apikey:{$apikey}"],http_build_query(["phone"=>$phone]));
+    }
+
+
 }
--- test/TestBaiduApi.php
+++ test/TestBaiduApi.php
@@ -5,5 +5,7 @@ require_once __DIR__.'/../src/BaiduApi.php';
 $Api = new \yishuixm\apistore\BaiduApi();
 
 
-print_r($Api->datatiny_cardinfo_cardinfo('',''));
-print_r($Api->apistore_idservice_id('',''));
+//print_r($Api->datatiny_cardinfo_cardinfo('',''));
+//print_r($Api->apistore_idservice_id('',''));
+
+print_r($Api->apistore_mobilenumber_mobilenumber('ba85fb304f7675b4ed27558f842951ef','18523054802'));
